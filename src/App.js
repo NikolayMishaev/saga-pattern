1c257223aab2f66d2cd92b788b130a59fca6516d
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2022-02-08 01:01:50 +0300

    feat: в проект подключены Redux и Redux Saga

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const store = useSelector((s) => s);
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          dispatch({ type: "ACTION" });
+        }}
+      >
+        Click
+      </button>
+    </div>
+  );
 }
 
 export default App;
